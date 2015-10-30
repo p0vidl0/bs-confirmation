fd1ec56a9516bf80f8c47b368bce814d371d4325
Aleksandr <nixomsk@gmail.com>
2015-10-30 16:04:45 +0600

    Update package.js

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: "tavicu:bs-confirmation",
-  version: "1.0.5",
+  name: "p0vidl0:bs-confirmation",
+  version: "1.0.6",
   summary: "Confirmation plugin compatible with Twitter Bootstrap 3 extending Popover",
-  git: "https://github.com/tavicu/bs-confirmation.git"
+  git: "https://github.com/p0vidl0/bs-confirmation.git"
 });
 
 Package.onUse(function(api) {
